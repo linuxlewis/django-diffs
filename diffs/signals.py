ff2cc0a7feccf2d0bc4d3a98e641c38e48cefe85
Sam Bolgert <sbolgert@gmail.com>
2016-10-24 16:57:59 -0700

    Add logic to skip diff when appropriate
    If the user_provided serialize_diff returns None or empty
    bail early
    
    * Add logging

--- diffs/signals.py
+++ diffs/signals.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import, unicode_literals
+import logging
 
 from django.core import serializers
 from django.db import connection
@@ -7,6 +8,8 @@ from django.db.models.signals import pre_save, post_save
 from .helpers import precise_timestamp
 
 from .settings import diffs_settings
+
+logger = logging.getLogger("diffs")
 
 
 def on_pre_save(sender, instance, **kwargs):
@@ -21,27 +24,32 @@ def on_post_save(sender, instance, created, **kwargs):
         else:
             data = serialize_object(instance, instance.__dirty_fields)
 
-        model = instance
-        # check if should be related to another "parent" model
-        if hasattr(instance, 'get_diff_parent'):
-            parent = instance.get_diff_parent()
-            if parent:
-                model = parent
+        if data:
+            model = instance
+            # check if should be related to another "parent" model
+            if hasattr(instance, 'get_diff_parent'):
+                parent = instance.get_diff_parent()
+                if parent:
+                    model = parent
 
-        create_kwargs = {
-            'data': data,
-            'created': created,
-            'pk': model.id,
-            'model_cls': model.__class__,
-            'timestamp': precise_timestamp()
-        }
-        # Respect the transaction if we can and should.
-        if hasattr(connection, 'on_commit') and diffs_settings['use_transactions']:
-            connection.on_commit(lambda: sender.diffs.create(**create_kwargs))
+            create_kwargs = {
+                'data': data,
+                'created': created,
+                'pk': model.id,
+                'model_cls': model.__class__,
+                'timestamp': precise_timestamp()
+            }
+            # Respect the transaction if we can and should.
+            if hasattr(connection, 'on_commit') and diffs_settings['use_transactions']:
+                connection.on_commit(lambda: sender.diffs.create(**create_kwargs))
+            else:
+                sender.diffs.create(**create_kwargs)
         else:
-            sender.diffs.create(**create_kwargs)
-
+            logger.debug("Skipped diff because it was emtpy.")
+        # clean up
         del instance.__dirty_fields
+    else:
+        logger.debug("Skipped diff because no fields had changed.")
 
 
 def serialize_object(instance, dirty_fields):
